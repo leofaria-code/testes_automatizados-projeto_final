04ee951c6d2c493fe129cae717d1871d404461e5
Leonardo Oliveira Faria <leofaria.email@gmail.com>
2023-05-10 21:46:40 -0300

    Projeto Enviado 2023-05-10 22:00

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,12 @@ ARG JAR_FILE=target/*.jar
 COPY $JAR_FILE app.jar
 ENTRYPOINT ["java","-jar", "/app.jar"]
 
-#WORKDIR /tmp
+WORKDIR /app
+
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+RUN ./mvnw dependency:resolve
+
+COPY src ./src
+
+CMD ["./mvnw", "spring-boot:run"]
--- src/main/java/br/com/ada/testeautomatizado/ProjetoFinalApplication.java
+++ src/main/java/br/com/ada/testeautomatizado/ProjetoFinalApplication.java
@@ -1,9 +1,12 @@
 package br.com.ada.testeautomatizado;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Veiculo API"))
 public class ProjetoFinalApplication {
 
 	public static void main(String[] args) {
